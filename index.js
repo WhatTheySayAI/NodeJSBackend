9a3b808297e266266455036251755af4de56d58c
hleo1 <leo.hubert3@gmail.com>
2022-12-09 15:02:59 -0500

    tutorial transcribe real time audio

--- index.js
+++ index.js
@@ -1,2 +1,37 @@
-require('dotenv').config();
-console.log(process.env.ASSEMBLY_API_KEY);
+require("dotenv").config();
+const axios = require("axios");
+const audioURL = "https://bit.ly/3yxKEIY";
+const APIKey = process.env.ASSEMBLY_API_KEY;
+const refreshInterval = 5000;
+
+const assembly = axios.create({
+  baseURL: "https://api.assemblyai.com/v2",
+  headers: {
+    authorization: APIKey,
+    "content-type": "application/json",
+  },
+});
+
+const getTranscript = async () => {
+    // Sends the audio file to AssemblyAI for transcription
+    const response = await assembly.post("/transcript", {
+      audio_url: audioURL,
+    });
+  
+    // Interval for checking transcript completion
+    const checkCompletionInterval = setInterval(async () => {
+      const transcript = await assembly.get(`/transcript/${response.data.id}`)
+      const transcriptStatus = transcript.data.status
+  
+      if (transcriptStatus !== "completed") {
+        console.log(`Transcript Status: ${transcriptStatus}`)
+      } else if (transcriptStatus === "completed") {
+        console.log("\nTranscription completed!\n")
+        let transcriptText = transcript.data.text
+        console.log(`Your transcribed text:\n\n${transcriptText}`)
+        clearInterval(checkCompletionInterval)
+      }
+    }, refreshInterval)
+  }
+  
+  getTranscript();
