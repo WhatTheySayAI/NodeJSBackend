1c6e2ad3ce2d57848541b225db3185c189fbb91d
BenGeordie <school@bengeordie@gmail.com>
2022-12-09 21:40:18 -0600

    backend done

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-require("dotenv").config();
-const axios = require("axios");
-const audioURL = "https://bit.ly/3yxKEIY";
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 const APIKey = process.env.ASSEMBLY_API_KEY;
-const refreshInterval = 5000;
 
-const assembly = axios.create({
-  baseURL: "https://api.assemblyai.com/v2",
-  headers: {
-    authorization: APIKey,
-    "content-type": "application/json",
-  },
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.post(
+        'https://api.assemblyai.com/v2/realtime/token',  // use account token to
+                                                         // get a temp user
+                                                         // token
+        {
+          expires_in: 3600,  // can set a TTL timer in seconds.
+          language_detection: true
+        },
+        {headers: {authorization: APIKey}});  // AssemblyAI API Key goes here
+    const {data} = response;
+    res.json(data);
+  } catch (error) {
+    const {response: {status, data}} = error;
+    res.status(status).json(data);
+  }
 });
 
-const getTranscript = async () => {
-    // Sends the audio file to AssemblyAI for transcription
-    const response = await assembly.post("/transcript", {
-      audio_url: audioURL,
-    });
-  
-    // Interval for checking transcript completion
-    const checkCompletionInterval = setInterval(async () => {
-      const transcript = await assembly.get(`/transcript/${response.data.id}`)
-      const transcriptStatus = transcript.data.status
-  
-      if (transcriptStatus !== "completed") {
-        console.log(`Transcript Status: ${transcriptStatus}`)
-      } else if (transcriptStatus === "completed") {
-        console.log("\nTranscription completed!\n")
-        let transcriptText = transcript.data.text
-        console.log(`Your transcribed text:\n\n${transcriptText}`)
-        clearInterval(checkCompletionInterval)
-      }
-    }, refreshInterval)
-  }
-  
-  getTranscript();
+app.set('port', 8000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server is running on port ${server.address().port}`);
+});
